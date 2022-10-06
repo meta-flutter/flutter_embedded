187a326037a25f2e59a295ffb0cff5cd2390aed5
Hannes Winkler <hanneswinkler2000@web.de>
2022-10-06 18:18:16 +0200

    Add normal .gclient file to be used by workflows

---
+++ dot-gclient.py
@@ -0,0 +1,10 @@
+solutions = [{
+  "managed": False,
+  "name": "src/flutter",
+  "url": "https://github.com/flutter/engine.git",
+  "deps_file": "DEPS",
+  "custom_vars": {
+    "download_linux_deps": True,
+    "download_android_deps": False
+  },
+}]
