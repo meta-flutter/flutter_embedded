87b101f7da184665fa212673009874336c21c4d2
Hannes Winkler <hanneswinkler2000@web.de>
2022-10-18 17:42:08 +0200

    Add integration test (#2)
    - more precise check if engine fetching needs dead libcxx repo workaround
    - fix dart frontend server invocation
    - only specify `gen_snapshot --sim-use-hardfp` for arm 32-bit
    - show verbose filetree
    - add test app
    - test on 64-bit too
    - use `GITHUB_OUTPUT` file
    - speed up channel watch

---
+++ test_app/README.md
@@ -0,0 +1,3 @@
+# test_app
+
+Just an empty test app that quits immediately with exit-code 0 after starting up.
---
+++ test_app/lib/main.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+
+void main() {
+  print('test app started up successfully. exiting...');
+  exit(0);
+}
